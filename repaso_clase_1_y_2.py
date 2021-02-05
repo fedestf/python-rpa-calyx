f661b7bb151183f87116788f8b095e15d4708416
fedestf <fede520@live.com>
2021-02-05 18:46:02 -0300

    Update repaso_clase_1_y_2.py

--- repaso_clase_1_y_2.py
+++ repaso_clase_1_y_2.py
@@ -1,4 +1,3 @@
-
 # Ejercicio 1
 
 numeros = [1, 2, 3, 4, 5]
@@ -32,7 +31,7 @@ dic_palabras = {}
 
 for palabra in range(0, len(palabras)):
     dic_palabras[palabras[palabra]] = (
-        'La palabra tiene {} letras'.format(len(palabras[palabra])))
+        'La palabra tiene {} letras'.format(len(palabras[palabra])))  # usar metodo replace para quitar espacios
 
 print(dic_palabras)
 
@@ -43,6 +42,7 @@ numeros_10 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 # Hacerlo desde el íncide 0 y el índice 1
 # Ejemplo: la tupla quedaría (1, 3, 5, 7, 9)
 
+# hacer con range asi no carga dos veces en memoria
 num_tupla_impar = tuple(numeros_10[0::2])
 num_tupla_par = tuple(numeros_10[0::2])
 print(num_tupla_impar, num_tupla_par)
@@ -70,8 +70,8 @@ diccionario = {
 lista_resultado = []
 
 for key in diccionario["acciones"]:  # Accedo a las keys del diccionario acciones
+    # con las keys del diccionario acciones paso el valor de los datos que tienen la misma key
     lista_resultado.append(
-        (diccionario["acciones"][key](diccionario["datos"][key])))  # con las keys del diccionario acciones paso el valor de los datos que tienen la misma key
-
+        (diccionario["acciones"][key](diccionario["datos"][key])))
 
 print(lista_resultado)
