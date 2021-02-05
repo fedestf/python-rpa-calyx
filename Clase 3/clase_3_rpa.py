62664cf6e366511ebcdff65b04877cae1022f7ab
fedestf <fede520@live.com>
2021-02-05 19:35:01 -0300

    update

---
+++ Clase 3/clase_3_rpa.py
@@ -0,0 +1,10 @@
+# Archivos
+# Manera correcta de abrir un archivo with open(ruta, modo) as file:
+
+
+# Excepciones funciones: TRACBACK,raise,assert
+# Dentro de un for siempre considerar si considero un try y un except
+
+import traceback
+
+traceback.format_exc()
