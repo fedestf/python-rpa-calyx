109a1830b01623e1d19b2aedf3561358ee034e96
fedestf <fede520@live.com>
2021-02-06 14:39:12 -0300

    Create ejercicio_3.py

---
+++ Clase 3/Ejercicios Clase 3/ejercicio_3.py
@@ -0,0 +1,20 @@
+# TODO: crear una función que dado un patrón busque todos los archivos que cumplan dicho patrón y los elimine
+# Dicha función debe poder ejecutarse dos veces sin problemas
+# Para resolver el ejercicio deberá investigar sobre las librerías "glob" y "os"
+import os.path
+import glob
+import os as file
+from os import remove
+
+# al poner el asterisco hago referencia a todos
+ruta = r'C:\Users\Federico\Documents\GitHub\python-rpa-calyx\Clase 3\Ejercicios Clase 3\*.txt'
+
+# Traigo una lista con la cantidad de archivos con extension .py en el directorio definido en ruta
+lista_txt = glob.glob(ruta)
+
+for txt in lista_txt:
+    try:
+        file.remove(txt)
+    except:
+        # Preguntar porque no entra a la excepcion
+        print("Los archivos no existen")
