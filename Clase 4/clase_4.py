6b1ebb784bb744359735892b115f57c436a09953
Federico López <fede520@live.com>
2021-02-08 20:15:47 -0300

    Create clase_4.py

---
+++ Clase 4/clase_4.py
@@ -0,0 +1,11 @@
+# leer sobre decordadores
+
+from modulos.modulo_1 import *
+
+# Para modulos se debe crear la carpeta modulos y el archivo __init__.py para que sean reconocidos los archivos en esa carpeta
+
+calcular_suma(2, 3)
+
+# Docstrings ''' poner variables a mostrar despues para guiarse '''
+
+# Para heredar es clase(clase a heredar)
