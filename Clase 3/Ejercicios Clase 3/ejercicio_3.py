8eb40ae8731e45a84776a5938d9d4ebb757c0151
fedestf <fede520@live.com>
2021-02-08 01:34:08 -0300

    Update ejercicio_3.py

--- Clase 3/Ejercicios Clase 3/ejercicio_3.py
+++ Clase 3/Ejercicios Clase 3/ejercicio_3.py
@@ -5,7 +5,8 @@ import glob
 import os as file
 from os import remove
 
-# al poner el asterisco hago referencia a todos
+# al poner el *.extension hago referencia a todos los archivos con determinada extension
+# Poniendo *.* traigo todos los archivos sin diferenciar extension
 ruta = r'C:\Users\Federico\Documents\GitHub\python-rpa-calyx\Clase 3\Ejercicios Clase 3\*.txt'
 
 # Traigo una lista con la cantidad de archivos con extension .py en el directorio definido en ruta
@@ -18,5 +19,4 @@ for txt in lista_txt:
         # Preguntar porque no entra a la excepcion en la segunda vuelta
         print("Los archivos no existen")
 
-for txt in lista_txt:
-    print(txt)
+print(lista_txt)
