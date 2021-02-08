923b8d7822f41b42706a9a1ba39929d9406aeb40
Federico López <fede520@live.com>
2021-02-08 18:54:27 -0300

    Update ejercicio_6.py

--- Clase 3/Ejercicios Clase 3/ejercicio_6.py
+++ Clase 3/Ejercicios Clase 3/ejercicio_6.py
@@ -1,7 +1,7 @@
 # TODO: crear una función que reciba la función dummy (y sus parámetros) e imprima por pantalla
 # El tiepmo que demoró en ejecutar dicha función
 # Para resolver el ejercicio deberá utilizar la librería "time"
-from datetime import datetime
+
 import time
 
 
@@ -12,15 +12,19 @@ def dummy(time_to_sleep):
         time.sleep(time_to_sleep)
 
 
-def calculo_tiempo_dummy(tiempo_dummy):
+def calculo_tiempo_dummy(tiempo_dummy):  # hacer funcion con dos parametros
 
-    tiempo_inicial = datetime.now()
+    tiempo_inicial = time.perf_counter()
 
     dummy(tiempo_dummy)
 
-    tiempo_final = datetime.now()
+    tiempo_final = time.perf_counter()
 
     print(tiempo_final - tiempo_inicial)
 
 
 calculo_tiempo_dummy(1)
+
+# Utiliza *args para pasar de forma opcional a una función un número variable de argumentos posicionales.
+# El parámetro *args recibe los argumentos como una tupla.
+# Emplea **kwargs para pasar de forma opcional a una función un número variable de argumentos con nombre.
