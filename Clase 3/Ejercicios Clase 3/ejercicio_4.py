7787b927851eba6c3dafbdbc2c0d0b450f4af526
fedestf <fede520@live.com>
2021-02-09 19:37:49 -0300

    Update ejercicio_4.py

--- Clase 3/Ejercicios Clase 3/ejercicio_4.py
+++ Clase 3/Ejercicios Clase 3/ejercicio_4.py
@@ -7,48 +7,33 @@ import os
 import shutil
 
 
-origen = r'C:\Users\Federico\Desktop\Python RPA Calyx'
-destino = r'C:\Users\Federico\Desktop\Python RPA Calyx\test'
+origen = r'C:\Users\Federico\Desktop\Origen'
+destino = r'C:\Users\Federico\Desktop\Destino'
 
 
 def mover_archivo(origen, destino):
 
     archivos_en_directorio = os.listdir(origen)
-    archivos_filtrados = []
-    cantidad_archivos = 0
 
     for archivo in archivos_en_directorio:
 
         try:
 
             if archivo.endswith('.py'):  # Filtro por extension
-                archivos_filtrados.append(archivo)
+
                 src = os.path.join(origen, archivo)
                 dst = os.path.join(destino, archivo)
                 shutil.move(src, dst)
-                cantidad_archivos += 1
                 print("Se movio correctamente el archivo : ", archivo)
 
-        except Exception as e:  # Esta excepcion es arrojada si no filtro por extension
-            print("El directorio contiene una carpeta : \n "+str(e))
+        # Esta excepcion es arrojada si no filtro por extension (sucedia cuando habia una carpeta de por medio)
+        except Exception as e:
+
+            print("Error : \n "+str(e))
             print("Los demas archivos ya se han movido")
 
 
 mover_archivo(origen, destino)
 
-
-# for archivo in archivos_en_directorio:
-
-#     try:
-
-#         src = os.path.join(origen, archivo)
-#         dst = os.path.join(destino, archivo)
-#         shutil.move(src, dst)
-#         print("Se movio correctamente el archivo : ", archivo)
-
-#     except Exception as e:
-
-#         print("El directorio contiene una carpeta : \n "+str(e))
-#         print("Los demas archivos ya se han movido")
 
 # https://docs.python.org/es/3/library/shutil.html
