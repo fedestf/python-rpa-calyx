2f18589cd7f07574318c87529108015f71c32e87
fedestf <fede520@live.com>
2021-02-09 19:37:53 -0300

    Update ejercicio_3.py

--- Clase 3/Ejercicios Clase 3/ejercicio_3.py
+++ Clase 3/Ejercicios Clase 3/ejercicio_3.py
@@ -7,17 +7,24 @@ from os import remove
 
 # al poner el *.extension hago referencia a todos los archivos con determinada extension
 # Poniendo *.* traigo todos los archivos sin diferenciar extension
-ruta = r'C:\Users\Federico\Documents\GitHub\python-rpa-calyx\Clase 3\Ejercicios Clase 3\*.txt'
+
 
 # Traigo una lista con la cantidad de archivos con extension .py en el directorio definido en ruta
-lista_txt = glob.glob(ruta)
 
-for txt in lista_txt:
-    try:
-        file.remove(txt)
 
-    except:
-        # Preguntar porque no entra a la excepcion en la segunda vuelta
-        print("Los archivos no existen")
+def borrar_archivo_ejercicio_clase_3(extension):
+    ruta = r'C:\Users\Federico\Documents\GitHub\python-rpa-calyx\Clase 3\Ejercicios Clase 3\*.txt'
+    lista_txt = glob.glob(ruta)
 
-print(lista_txt)
+    for txt in lista_txt:
+        if txt.endswith(extension):
+            try:
+                file.remove(txt)
+                print(f"El archivo {txt} ha sido eliminado")
+
+            except:
+                # Preguntar porque no entra a la excepcion en la segunda vuelta
+                print("Los archivos no existen")
+
+
+borrar_archivo_ejercicio_clase_3(".txt")
