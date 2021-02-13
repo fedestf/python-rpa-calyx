864dfcf806402223ddced590e7fcbd41745b3643
Federico López <fede520@live.com>
2021-02-13 03:48:50 -0300

    Update libreria_logging.py

--- Clase 6/libreria_logging.py
+++ Clase 6/libreria_logging.py
@@ -10,21 +10,35 @@ from datetime import date
 # Custom logger
 
 # Creo subdirectorio (en caso de que no exista) a partir de la ruta de logs definida en settings
-# carpeta_error = os.path.join(RUTA, "Error")
+# carpeta_error = os.path.join(RUTA, "Nombre carpeta")
 
 # # Declaro la ruta  y el nombre del archivo para pasarlo al file handler
-# # Ahora mi ruta es carpeta
+# # Ahora mi ruta es la carpeta creada antes
+
+# ruta = os.path.join(carpeta, date.today().strftime("%d-%m-%Y")+".txt")
+
+# logger_custom.info("INFO")
+# logger_custom.debug("DEBUG")
+# logger_custom.critical("CRITICAL")
+# logger_custom.error("ERROR")
+# logger_custom.warning("WARNING")
+
+# ----------------------TEST ERROR-------------------------
 def error_test(error):
 
     carpeta_error = os.path.join(RUTA, "Error")
     if not os.path.exists(carpeta_error):
         os.makedirs(carpeta_error)
 
-    ruta = os.path.join(carpeta_error, str(date.today()))
-    logger_custom = logging.getLogger("test_custom")
+    ruta = os.path.join(carpeta_error,
+                        date.today().strftime("%d-%m-%Y") + ".txt")
+
+    logger_custom = logging.getLogger("test_error")
     file_hand = logging.FileHandler(ruta)
+
     format_hand = logging.Formatter(
-        '%(levelname)s : %(asctime)s - %(message)s')
+        '%(levelname)s : %(asctime)s.%(msecs)03d -  [%(funcName)s]- %(message)s', "%d-%m-%Y %H:%M:%S")
+
     file_hand.setFormatter(format_hand)
     logger_custom.addHandler(file_hand)
     logger_custom.exception(error)
@@ -34,24 +48,22 @@ def error_test(error):
 
 
 def debug_test(mensaje):
+
     carpeta_debug = os.path.join(RUTA, "Debug")
 
     if not os.path.exists(carpeta_debug):
         os.makedirs(carpeta_debug)
 
-    ruta = os.path.join(carpeta_debug, str(date.today()))
+    ruta = os.path.join(carpeta_debug,
+                        date.today().strftime("%d-%m-%Y") + ".txt")
+
     logger_custom = logging.getLogger("test_debug")
     file_hand = logging.FileHandler(ruta)
+
     format_hand = logging.Formatter(
-        '%(levelname)s : %(asctime)s - %(message)s')
+        '%(levelname)s : %(asctime)s.%(msecs)03d -  [%(funcName)s]- %(message)s', "%d-%m-%Y %H:%M:%S")
+
     file_hand.setFormatter(format_hand)
     logger_custom.addHandler(file_hand)
     logger_custom.setLevel(logging.DEBUG)
     logger_custom.debug(mensaje)
-
-
-# logger_custom.info("INFO")
-# logger_custom.debug("DEBUG")
-# logger_custom.critical("CRITICAL")
-# logger_custom.error("ERROR")
-# logger_custom.warning("WARNING")
