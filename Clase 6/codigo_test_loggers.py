b8f59b2f45bd474fba08aaa993b1e64edd74da39
Federico López <fede520@live.com>
2021-02-13 04:19:39 -0300

    Update codigo_test_loggers.py

--- Clase 6/codigo_test_loggers.py
+++ Clase 6/codigo_test_loggers.py
@@ -1,33 +1,36 @@
 from libreria_logging import debug_test, error_test
 
-# try:
-#     1+"sss"
-#     debug_test("Ejecutado correctamente")
-# except Exception as e:
-#     error_test(e)
+try:
+    1+"sss"
+    debug_test("Ejecutado correctamente")
+except Exception as e:
+    error_test(e)
 
-# try:
-#     1+2
-#     debug_test("VAPAI")
-# except Exception as e:
-#     error_test(e)
+try:
+    1+2
+    debug_test("VAPAI")
+except Exception as e:
+    error_test(e)
 
 
-# def suma_prueba(num1, num2):
+def suma_prueba(num1, num2):
+    try:
+        resultado = num1 + num2
+        debug_test("asdadasd")
+        return resultado
+    except Exception as e:
+        error_test(e)
+
+
+suma_prueba(2, 2)
+
+# lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# for i in lista:
 #     try:
-#         resultado = num1 + num2
-#         debug_test("asdadasd")
-#         return resultado
+#         print(i+"s")
+#         debug_test("Se ejecuto bien hasta {} ".format(i))
 #     except Exception as e:
 #         error_test(e)
 
-
-# print(suma_prueba(2, 2))
-lista = [1, "GASasA", 2, "GASasA", 3, 4, 5, "GASasA"]
-
-for elemento in lista:
-    try:
-        print(elemento+"KK")
-        debug_test("Se ejecuto bien hasta {} ".format(elemento[0]))
-    except Exception as e:
-        error_test(e)
+# Preguntar porque empieza a escribir mas de una vez las cosas cuando deberia escribirla solamente una vez
